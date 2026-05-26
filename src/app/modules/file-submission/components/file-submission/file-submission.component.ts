255105fdffed68a84890092d8a1aabb4ed50ae74
aniyariyavijay <vijaykumar.aniyaliya@tatvasoft.com>
2026-05-26 14:06:48 +0530

    Made changes to the AI evaluation screen UI

---
+++ src/app/modules/file-submission/components/file-submission/file-submission.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-submission',
+  imports: [],
+  templateUrl: './file-submission.component.html',
+  styleUrl: './file-submission.component.scss'
+})
+export class FileSubmissionComponent {
+
+}
--- src/app/modules/file-submission/submission.routes.ts
+++ src/app/modules/file-submission/submission.routes.ts
@@ -4,6 +4,8 @@ export const submissionRoutes: Routes = [
   {
     path: 'file-submission',
     loadComponent: () =>
-      import('./components/file-submission').then(m => m.FileSubmission)
-  }
+      import('./components/file-submission/file-submission.component').then(
+        (m) => m.FileSubmissionComponent,
+      ),
+  },
 ];
